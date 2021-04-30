aa1dc26858fcb24eca1267bc605a68f816cd59e4
Rodrigo Hortega <hortega.rodrigo@gmail.com>
2021-05-01 00:12:07 +0100

    Rotas contract

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  # devise_scope :user do
-  #   get 'users/:id' => 'developer_profiles#show', as: user
-  # end
-
+ 
   resources :users, :only => [:show] do
     resources :developer_profiles
   end
-
-
-  # resources :users do
-  #   resources :developer_profiles
-  # end
-  # devise_scope :user do
-  # get 'users/:id' => 'registrations#show', as: user
-  # end
-
+  
   root to: 'pages#home'
+  resources :contracts, only: [:show, :new, :create, :destroy]
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
